278dd0ae240b0b31c1f2099811ffe2ef898e1b22
dom-devel <dom_woodman@msn.com>
2019-04-11 22:16:15 +0100

    remove other reqs

--- setup.py
+++ setup.py
@@ -13,29 +13,27 @@ def get_version(package):
     """
     Return package version as listed in `__version__` in `init.py`.
     """
-    with open(os.path.join(package, '__init__.py'), 'rb') as init_py:
-        src = init_py.read().decode('utf-8')
+    with open(os.path.join(package, "__init__.py"), "rb") as init_py:
+        src = init_py.read().decode("utf-8")
         return re.search("__version__ = ['\"]([^'\"]+)['\"]", src).group(1)
 
 
-name = 'django-rest-logger'
-version = get_version('django_rest_logger')
-package = 'django_rest_logger'
-description = 'REST logger for Django'
-url = 'https://github.com/Seedstars/django-rest-logger'
-author = 'Pedro Gomes'
-author_email = 'pedro@seedstars.com'
-license = 'MIT'
-install_requires = [
-    'Django>=1.11,<2.0',
-]
+name = "django-rest-logger"
+version = get_version("django_rest_logger")
+package = "django_rest_logger"
+description = "REST logger for Django"
+url = "https://github.com/Seedstars/django-rest-logger"
+author = "Pedro Gomes"
+author_email = "pedro@seedstars.com"
+license = "MIT"
+install_requires = ["Django>=1.11"]
 
 
 def read(*paths):
     """
     Build a file path from paths and return the contents.
     """
-    with open(os.path.join(*paths), 'r') as f:
+    with open(os.path.join(*paths), "r") as f:
         return f.read()
 
 
@@ -43,9 +41,11 @@ def get_packages(package):
     """
     Return root package and all sub-packages.
     """
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+    return [
+        dirpath
+        for dirpath, dirnames, filenames in os.walk(package)
+        if os.path.exists(os.path.join(dirpath, "__init__.py"))
+    ]
 
 
 def get_package_data(package):
@@ -53,32 +53,33 @@ def get_package_data(package):
     Return all files under the root package, that are not in a
     package themselves.
     """
-    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-            for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+    walk = [
+        (dirpath.replace(package + os.sep, "", 1), filenames)
+        for dirpath, dirnames, filenames in os.walk(package)
+        if not os.path.exists(os.path.join(dirpath, "__init__.py"))
+    ]
 
     filepaths = []
     for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename)
-                          for filename in filenames])
+        filepaths.extend([os.path.join(base, filename) for filename in filenames])
     return {package: filepaths}
 
 
-if sys.argv[-1] == 'publish':
-    if os.system('pip freeze | grep wheel'):
-        print('wheel not installed.\nUse `pip install wheel`.\nExiting.')
+if sys.argv[-1] == "publish":
+    if os.system("pip freeze | grep wheel"):
+        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
         sys.exit()
-    if os.system('pip freeze | grep twine'):
-        print('twine not installed.\nUse `pip install twine`.\nExiting.')
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
         sys.exit()
-    os.system('python setup.py sdist bdist_wheel')
-    os.system('twine upload dist/*')
-    shutil.rmtree('dist')
-    shutil.rmtree('build')
-    shutil.rmtree('django_rest_logger.egg-info')
-    print('You probably want to also tag the version now:')
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    shutil.rmtree("dist")
+    shutil.rmtree("build")
+    shutil.rmtree("django_rest_logger.egg-info")
+    print("You probably want to also tag the version now:")
     print("  git tag -a {0} -m 'version {0}'".format(version))
-    print('  git push --tags')
+    print("  git push --tags")
     sys.exit()
 
 
@@ -88,25 +89,25 @@ setup(
     url=url,
     license=license,
     description=description,
-    long_description=read('README.rst'),
+    long_description=read("README.rst"),
     author=author,
     author_email=author_email,
     packages=get_packages(package),
     package_data=get_package_data(package),
     install_requires=install_requires,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Topic :: Internet :: WWW/HTTP',
-    ]
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
 )
